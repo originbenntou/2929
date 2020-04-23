dac7f9cff232815f5e7d0e065d37d53e52ef479d
originbenntou <originbenntou8973@gmail.com>
2020-04-24 01:38:49 +0900

    mysql init

--- mysql/init/00.account.sql
+++ mysql/init/00.account.sql
@@ -3,6 +3,26 @@ CREATE DATABASE account DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
 
 use account;
 
+DROP TABLE IF EXISTS plan;
+CREATE TABLE plan (
+  id INT unsigned NOT NULL auto_increment,
+  name VARCHAR(255) NOT NULL,
+  price INT unsigned NOT NULL DEFAULT 0,
+  capacity INT unsigned  NOT NULL DEFAULT 3,
+  PRIMARY KEY (id)
+) COMMENT 'プラン情報';
+
+DROP TABLE IF EXISTS company;
+CREATE TABLE company (
+  id INT unsigned NOT NULL auto_increment,
+  name VARCHAR(255) NOT NULL,
+  plan_id INT unsigned NOT NULL,
+  created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (id),
+  FOREIGN KEY (plan_id) REFERENCES plan(id)
+) COMMENT '会社情報';
+
 DROP TABLE IF EXISTS user;
 CREATE TABLE user (
   id INT unsigned NOT NULL auto_increment,
@@ -16,23 +36,3 @@ CREATE TABLE user (
   UNIQUE KEY (email),
   FOREIGN KEY (company_id) REFERENCES company(id)
 ) COMMENT 'ユーザー情報';
-
-DROP TABLE IF EXISTS company;
-CREATE TABLE company (
-  id INT unsigned NOT NULL auto_increment,
-  name VARCHAR(255) NOT NULL,
-  plan_id INT unsigned NOT NULL,
-  created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (id),
-  FOREIGN KEY (plan_id) REFERENCES plan(id)
-) COMMENT '会社情報';
-
-DROP TABLE IF EXISTS plan;
-CREATE TABLE plan (
-  id INT unsigned NOT NULL auto_increment,
-  name VARCHAR(255) NOT NULL,
-  price INT unsigned NOT NULL DEFAULT 0,
-  capacity INT unsigned  NOT NULL DEFAULT 3,
-  PRIMARY KEY (id)
-) COMMENT 'プラン情報';
