9078f159599fd643e52af42e51cd8b5e3ee55ce6
originbenntou <originbenntou8973@gmail.com>
2020-04-29 18:01:36 +0900

    db comment追加

--- mysql/init/00.account.sql
+++ mysql/init/00.account.sql
@@ -5,33 +5,33 @@ use account;
 
 DROP TABLE IF EXISTS plan;
 CREATE TABLE plan (
-  id INT unsigned NOT NULL auto_increment,
-  name VARCHAR(255) NOT NULL,
-  price INT unsigned NOT NULL DEFAULT 0,
-  capacity INT unsigned  NOT NULL DEFAULT 3,
+  id INT unsigned NOT NULL auto_increment COMMENT 'プランID',
+  name VARCHAR(255) NOT NULL COMMENT 'プラン名',
+  price INT unsigned NOT NULL DEFAULT 0 COMMENT 'プラン金額',
+  capacity INT unsigned  NOT NULL DEFAULT 3 COMMENT 'ログイン上限',
   PRIMARY KEY (id)
 ) COMMENT 'プラン情報';
 
 DROP TABLE IF EXISTS company;
 CREATE TABLE company (
-  id INT unsigned NOT NULL auto_increment,
-  name VARCHAR(255) NOT NULL,
-  plan_id INT unsigned NOT NULL,
-  created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  id INT unsigned NOT NULL auto_increment COMMENT '会社ID',
+  name VARCHAR(255) NOT NULL COMMENT '会社名',
+  plan_id INT unsigned NOT NULL COMMENT 'プランID',
+  created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '作成日時',
+  updated_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新日時',
   PRIMARY KEY (id),
   FOREIGN KEY (plan_id) REFERENCES plan(id)
 ) COMMENT '会社情報';
 
 DROP TABLE IF EXISTS user;
 CREATE TABLE user (
-  id INT unsigned NOT NULL auto_increment,
-  email VARCHAR(255) NOT NULL,
-  password VARCHAR(1023) NOT NULL,
-  name VARCHAR(255) NOT NULL,
-  company_id INT unsigned NOT NULL,
-  created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  id INT unsigned NOT NULL auto_increment COMMENT 'ユーザーID',
+  email VARCHAR(255) NOT NULL COMMENT 'ユーザーEmailアドレス',
+  password VARCHAR(1023) NOT NULL COMMENT 'ユーザーパスワードハッシュ',
+  name VARCHAR(255) NOT NULL COMMENT 'ユーザー名',
+  company_id INT unsigned NOT NULL COMMENT '会社ID',
+  created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '作成日時',
+  updated_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新日時',
   PRIMARY KEY (id),
   UNIQUE KEY (email),
   FOREIGN KEY (company_id) REFERENCES company(id)
@@ -39,12 +39,12 @@ CREATE TABLE user (
 
 DROP TABLE IF EXISTS session;
 CREATE TABLE session (
-  id INT unsigned NOT NULL auto_increment,
-  token VARCHAR(255) NOT NULL,
-  user_id INT unsigned NOT NULL,
-  company_id INT unsigned NOT NULL,
-  created_at datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
-  updated_at datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
+  id INT unsigned NOT NULL auto_increment COMMENT 'セッションID',
+  token VARCHAR(255) NOT NULL COMMENT 'セッショントークン',
+  user_id INT unsigned NOT NULL COMMENT 'ユーザーID',
+  company_id INT unsigned NOT NULL COMMENT '会社ID',
+  created_at datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) COMMENT '作成日時（マイクロ秒）',
+  updated_at datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6) COMMENT '更新日時（マイクロ秒）',
   PRIMARY KEY (id),
   FOREIGN KEY (user_id) REFERENCES user(id),
   FOREIGN KEY (company_id) REFERENCES company(id)
--- mysql/init/00.trend.sql
+++ mysql/init/00.trend.sql
@@ -5,23 +5,23 @@ use trend;
 
 DROP TABLE IF EXISTS suggest;
 CREATE TABLE suggest (
-  id INT unsigned NOT NULL auto_increment,
-  keyword VARCHAR(255) NOT NULL,
-  date DATE NOT NULL,
-  result json DEFAULT NULL,
-  status tinyint(1) NOT NULL DEFAULT 0,
-  created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  id INT unsigned NOT NULL auto_increment COMMENT 'サジェストID',
+  keyword VARCHAR(255) NOT NULL COMMENT 'トレンド検索キーワード',
+  date DATE NOT NULL COMMENT '検索開始日',
+  result json DEFAULT NULL COMMENT 'トレンド検索結果',
+  status tinyint(1) NOT NULL DEFAULT 0 COMMENT 'トレンド検索進捗',
+  created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '作成日時',
+  updated_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新日時',
   PRIMARY KEY (id),
   UNIQUE KEY (keyword, date)
-) COMMENT 'サジェストキーワード';
+) COMMENT 'サジェスト情報';
 
 DROP TABLE IF EXISTS history;
 CREATE TABLE history (
-  id INT unsigned NOT NULL auto_increment,
-  user_id INT unsigned NOT NULL,
-  suggest_id INT unsigned NOT NULL,
-  created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  id INT unsigned NOT NULL auto_increment COMMENT 'トレンド検索履歴ID',
+  user_id INT unsigned NOT NULL COMMENT 'ユーザーID',
+  suggest_id INT unsigned NOT NULL COMMENT 'サジェストID',
+  created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '作成日時',
   PRIMARY KEY (id),
   UNIQUE KEY (user_id, suggest_id),
   FOREIGN KEY (user_id) REFERENCES account.user(id),
